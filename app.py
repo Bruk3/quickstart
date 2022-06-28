5224392f2cab17ff5a56feac2d4b6146f7abba3b
Bruk <bbz2103@columbia.edu>
2022-06-28 00:13:11 +0000

    specify templates folder

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def hello_world():
-    return render_template('./templates/home.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
