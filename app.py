741bbf70e08f116d9967b57fde10bc08166f7752
Bruk <bbz2103@columbia.edu>
2022-06-28 00:20:56 +0000

    correct template dir

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():
