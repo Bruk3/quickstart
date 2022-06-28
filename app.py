f624f981ea6c546ba636ed97ae4037f2e5ac85a7
Bruk <bbz2103@columbia.edu>
2022-06-28 08:22:51 +0000

    render quote by stubbing call to quote service

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 import os
+import urllib.request
 
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
+QUOTE_SERVICE_URL = os.environ.get("QUOTE_SERVICE_URL")
+PORT = os.environ.get("QUOTE_SERVICE_PORT")
+
+def fetch_quote():
+    contents = urllib.request.urlopen(
+            "http://{QUOTE_SERVICE_URL}:{PORT}/randomquote".format(
+                QUOTE_SERVICE_URL=QUOTE_SERVICE_URL,
+                PORT=PORT
+            )
+        ).read()
+
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
+@app.route('/quote')
+def get_quote():
+    print ("Printing Quote of the Day")
+    # quote = fetch_quote()
+    return render_template(
+            'home.html', 
+            quote="People say nothing is impossible, but I do nothing every day.",
+            by="Winnie the Pooh"
+        )
+
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8082)))
